6c9762339fd414d26ae67d05a93844b19a35dec3
martinBosch <martinbosch17@gmail.com>
2018-09-27 23:19:16 -0300

    Errorres mas detallados al registrarse o loguearse

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from flask import request, redirect, url_for, jsonify
+import json
 from ..settings import error_handler, response_handler
 from ..settings import config
 import pyrebase
@@ -11,8 +12,7 @@ class Sign_Up(Resource):
         self.firebase = kwargs.get('firebase')
 
     def post(self):
-
-#        try:
+        try:
             json_data = request.get_json(force=True)
             self.logger.info(json_data)
             email = json_data['email']
@@ -26,9 +26,9 @@ class Sign_Up(Resource):
             response = response_handler.Response_Handler(config.Config.CODE_OK, response_data)
             return response.getResponse()
 
-#        except pyrebase.pyrebase.HTTPError:
-#            error = error_handler.Error_Handler(config.Config.CODE_BAD_REQUEST, 'Datos incorrectos. Intente de nuevo.')
-#            return error.getErrorResponse()
+        except pyrebase.pyrebase.HTTPError:
+            error = error_handler.Error_Handler(config.Config.CODE_BAD_REQUEST, 'Datos incorrectos. Intente de nuevo.')
+            return error.getErrorResponse()
 
  
 class Login(Resource):
@@ -43,7 +43,6 @@ class Login(Resource):
         password = json_data['password']
         
         auth = self.firebase.auth()
-
         try:
             user = auth.sign_in_with_email_and_password(email, password)
 
@@ -51,8 +50,10 @@ class Login(Resource):
             response = response_handler.Response_Handler(config.Config.CODE_OK, response_data)
             return response.getResponse()
 
-        except pyrebase.HTTPError as e:
-            error = error_handler.Error_Handler(config.Config.CODE_BAD_REQUEST, 'Datos incorrectos. Intente de nuevo. Detalles: ' + e)
+        except pyrebase.pyrebase.HTTPError as e:
+            error_json = e.args[1]
+            error_message = json.loads(error_json)["error"]["message"]
+            error = error_handler.Error_Handler(config.Config.CODE_BAD_REQUEST, error_message)
             return error.getErrorResponse()
 
         # return redirect(url_for('hello'))
