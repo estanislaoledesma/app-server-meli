73427658da083c8e5f587eef57c09fad9bc8c753
Estanislao Ledesma <estanislaomledesma@gmail.com>
2018-11-05 03:54:40 -0300

    Método ping

---
+++ src/routes/ping.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+from flask import jsonify
+from ..settings import responsehandler
+from flask_api import status
+
+class HelloWorld(Resource):
+
+    def __init__(self, **kwargs):
+        self.logger = kwargs.get('logger')
+
+    def get(self):
+        response_data = {'Ping': 'Pong'}
+        response = responsehandler.ResponseHandler(status.HTTP_200_OK, response_data)
+        return response.get_response()
