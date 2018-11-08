8a3f68153d7c54805db36cb724d2b62e45fb26a6
martinBosch <martinbosch17@gmail.com>
2018-11-08 13:25:08 -0300

    Fix error

--- src/routes/products.py
+++ src/routes/products.py
@@ -38,7 +38,7 @@ class Products(Resource):
                 product_to_display = {}
                 product_to_display['name'] = product['name']
                 product_to_display['price'] = product['price']
-                # product_to_display ['thumbnail'] = self.encode_image(product ['images'] [THUMBNAIL])
+                product_to_display ['thumbnail'] = self.encode_image(product ['images'] [THUMBNAIL])
                 product_to_display['_id'] = str(product['_id'])
                 products.append(product_to_display)
 
